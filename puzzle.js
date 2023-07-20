5ced21bc52452874677522ad1f28ba1f32723020
Khushi rajpurohit <rajpurohitkhushi36@gmail.com>
2023-07-20 23:05:12 +0530

    adding files on O1

--- puzzle.js
+++ puzzle.js
@@ -1,8 +1,14 @@
-const  pieces = document.querySelectorAll('.piece1.piece2.piece3.piece4.piece5.piece6.piece7.piece8.piece9');
-        .pieces.addEventListner("dragStart",()=>{
-            console.log('drag event triggered');
-        })
+let pieces = document.querySelectorAll("p");
 
-        .pieces.addEventListner("dragEnd",()=>{
-            console.log('drag event ended');
-        })
+function dragStart(event) {
+    addEventListener('dragstart',()=>{
+      console.log(document.getElementById("p"));
+    })
+  }
+  
+  function allowDrop(event) {
+;
+  }
+  
+
+  
